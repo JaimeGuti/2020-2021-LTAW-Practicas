bfe56f1e766151ee36145c55a718758ed77ef19d
JaimeGuti <jaime.m.guti@gmail.com>
2021-03-04 01:42:13 +0100

    Añadida constante con el valor del puerto

--- P1/S2/server-01.js
+++ P1/S2/server-01.js
@@ -1,6 +1,9 @@
 //Importar módulo http
 const http = require('http');
 
+// Definir el puerto a utilizar
+const PUERTO = 8080;
+
 //-- Crear el servidor
 const server = http.createServer();
 
@@ -20,6 +23,8 @@ function atender(req, res) {
 //-- Activar la función de retrollamada del servidor
 server.on('request', atender);
 
-//-- Activar el servidor. A la escucha de peitciones
+//-- Activar el servidor. A la escucha de peticiones
 //-- en el puerto 8080
-server.listen(8080);
+server.listen(PUERTO);
+
+console.log("Servido activado. Escuchando en el puerto: " + PUERTO);
