1ba051d97ee414eb0ca42716d2a9a168f79e6631
JaimeGuti <jaime.m.guti@gmail.com>
2021-03-04 01:34:03 +0100

    Añadida función de retrollamada al servidor

--- P1/S2/server-01.js
+++ P1/S2/server-01.js
@@ -1,7 +1,21 @@
+//Importar módulo http
 const http = require('http');
 
-for(p in http){
-    console.log("Propiedad " + p);
+//-- Crear el servidor
+const server = http.createServer();
+
+//-- Función de retrollamada de petición recibida
+//-- Cada vez que un cliente realiza una petición
+//-- Se llama a esta función
+function atender(req, res) {
+    //-- req: http.IncomingMessage: Mensaje de solicitud
+    //-- res: http.ServerResponse: Mensaje de respuesta (vacío)
+
+    //-- Indicamos que se ha recibido una petición
+    console.log("Petición recibida!");
+
+    //-- pero no enviamos respuesta (todavía)
 }
 
-http.createServer
+//-- Activar la función de retrollamada del servidor
+server.on('request', atender);
