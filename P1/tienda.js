4e9f9a3439022ede1f6bd32f00180c71a62ef87c
JaimeGuti <jaime.m.guti@gmail.com>
2021-04-01 02:03:03 +0200

    Usando módulo fs para lectura de ficheros

--- P1/tienda.js
+++ P1/tienda.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 //-- Definir el puerto a utilizar
 const PUERTO = 9000;
 
-//const pagina_main = PAG;
-//const pagina_error = IMG;
+const pagina_main = fs.readFileSync("tienda.html");
+const pagina_error = fs.readFileSync("error.html");
 
 //-- Imprimir información sobre el mensaje de solicitud
 function print_info_req(req) {
@@ -20,12 +20,6 @@ function print_info_req(req) {
   console.log("Método: " + req.method);
   console.log("Recurso: " + req.url);
   console.log("Version: " + req.httpVersion)
-  /*console.log("Cabeceras: ");
-
-  //-- Recorrer todas las cabeceras disponibles
-  //-- imprimiendo su nombre y su valor
-  for (hname in req.headers)
-    console.log(`  * ${hname}: ${req.headers[hname]}`);*/
 
   //-- Construir el objeto url con la url de la solicitud
   const myURL = new URL(req.url, 'http://' + req.headers['host']);
@@ -42,43 +36,38 @@ function print_info_req(req) {
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
     
-  //-- Indicamos que se ha recibido una petición
+  //-- Imprimir que se ha recibido una petición
   console.log("");
   console.log("PETICIÓN RECIBIDA");
-  //-- Petición recibida
+  
   //-- Imprimir información de la petición
   print_info_req(req);
 
-    //-- Valores de la respuesta por defecto
-    let code = 200;
-    let code_msg = "OK";
-    //let page = pagina_main;
-    let page = "200 OK"; // Solo para probarlo
-
-    //-- Analizar el recurso
-    //-- Construir el objeto url con la url de la solicitud
-    const url = new URL(req.url, 'http://' + req.headers['host']);
- //   console.log(url.pathname);
-
-    //-- Cualquier recurso que no sea la página principal
-    //-- genera un error
-    if (url.pathname != '/') {
+    const myURL = new URL(req.url, 'http://' + req.headers['host']);
+    const recurso = myURL.pathname;
+    fs.readFile(recurso, function(err, page){
+      //-- Valores de la respuesta por defecto
+      let code = 200;
+      let code_msg = "OK";
+      page = pagina_main;
+      //-- Cualquier recurso que no exista genera un error
+      if (recurso != '/'){ // CAMBIAR
         code = 404;
         code_msg = "Not Found";
-        //page = pagina_error;
-        page = "404 Not Found"; // Solo para probarlo
-    }
+        page = pagina_error;
+      }
 
-    //-- Generar la respuesta en función de las variables
-    //-- code, code_msg y page
-    res.statusCode = code;
-    res.statusMessage = code_msg;
-    res.setHeader('Content-Type','text/html');
-    res.write(page);
-    res.end();
+      //-- Generar la respuesta en función de las variables
+      //-- code, code_msg y page
+      res.statusCode = code;
+      res.statusMessage = code_msg;
+      res.setHeader('Content-Type','text/html');
+      res.write(page);
+      res.end();
+    })
 });
 
-//-- Activar el servidor: ¡Que empiece la fiesta!
+//-- Activar el servidor:
 server.listen(PUERTO);
 
 console.log("Servidor activado. Escuchando en puerto: " + PUERTO);
